3505c1b25af456851019daf0384af2dd7e07afaa
Josiah Charley <josiah.charley@student.awesomeinc.org>
2023-03-23 19:27:59 +0000

    added update function

---
+++ README.md
@@ -0,0 +1,40 @@
+CRUD
+
+Adding functions 
+List All info function:
+- 
+
+
+
+
+
+
+New Hero Func:
+-Propt user for inputs bio, name, etc
+- append to table of heroes
+- Prompt for abilities 
+    -if yes: 
+        - diplay all abilites you can add to heroes
+        - insert list of heroes sepearted by comma 
+        - have a for loop that goes through each ability and executes and adds it to current hero selected 
+
+add abilites func: function can be used on its own and is used if prompted in creating a new hero 
+ - take two parameters: location to add and values
+
+
+
+
+
+ Init:
+ 1: Prompt user for actions
+    Available actions:
+     1.) List all heroes
+     2.) List all heroes and their abilities
+     3.) Add new Hero
+     4.) Delete hero 
+     5.) Modify Hero 
+        -Print all heros and select based on Id
+        -user input: id(needs to be stored to plug into functions)
+        -ask what you want to edit about the hero(name,bio,abilities etc)
+        
+        
--- src/main.py
+++ src/main.py
@@ -1,12 +1,27 @@
-from database.db_connection import execute_query
+from database.db_connection import execute_query,create_connection
 
 
 
-def listnames():
+def listallInfo():
 
-    names = execute_query("SELECT id,name FROM heroes")
-    for i in names:
-        print(i)
+    basic_info_query = """
+        SELECT id,name FROM heroes
+    """
+    abilities_query = f"""
+        SELECT hero_id, name 
+        FROM abilities 
+        JOIN ability_types 
+        ON abilities.ability_type_id = ability_types.id
+    """
+    basic_info = execute_query(basic_info_query).fetchall()
+    abilities = execute_query(abilities_query).fetchall()
+    for hero in basic_info:
+        ability = []
+        for x in abilities:
+            
+            if x[0] == hero[0]:
+                ability.append(x[1])
+        print(f"{hero[0]} {hero[1]} {ability}")
 
 def getInfo():
     listnames()
@@ -15,20 +30,53 @@ def getInfo():
     for i in names:
         print(i)
 
+
+
 def newHero():
     name = input("Enter new hero name")
+    about = input(f"Enter {name}'s about me text.")
+    biography = input(f"Enter {name}'s biography ")
+    quer = """
+    INSERT INTO heroes (name,about_me,biography)
+    VALUES(%s,%s,%s)
+    """
+    execute_query(quer,(name,about,biography))
 
-    Insert
+    input()
+
+
+
+run = True
+
+
+#while run:
+#    newHero()
+
+
+def listnames():
+    query = """
+        SELECT id,name FROM heroes
+    """
+    returned_items = execute_query(query).fetchall()
+    for i in returned_items:
+        print(f"{i[0]}) {i[1]}")
+
+
+def updateName(id):
+    name = input("what would you like to change about this hero 1) name 2)abilities 3)")
+    
+    
+    quer = f"""
+    INSERT INTO heroes (name,about_me,biography)
+    VALUES(%s,)
+    """
+    execute_query(quer,(name,about,biography))
+
+
+
 
 def prompt():
     option = int(input("What would you like to do?"))
     
     if option == 1:
         yee()
-
-
-run = True
-
-
-while run:
-    prompt()
